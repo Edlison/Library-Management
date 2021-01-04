8a87e9224762139d164451fa3a5e000789dee568
ad-zt <ncepuzwq@163.com>
2021-01-04 10:16:39 +0800

    version 1.3

--- backend/api/catalog/models/catalog_model.py
+++ backend/api/catalog/models/catalog_model.py
@@ -13,7 +13,7 @@ class Catalog(db.Model):
     book_remainder_num = db.Column(db.Integer,nullable = True)
     book_state = db.Column(db.Integer,nullable = True)
     book_catalog_time = db.Column(db.DateTime, default=datetime.now)
-    book_return_reason = db.Column(db.String(50))
+    book_return_reason = db.Column(db.String(200))
 
     def keys(self):
         return ['catalog_id',
