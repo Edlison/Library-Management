b2577101632843e81ed441feb3ae803fea718b6f
ad-zt <ncepuzwq@163.com>
2021-01-05 12:12:32 +0800

    version 1.5

---
+++ backend/api/catalog/mapper/return_SQL.py
@@ -0,0 +1,49 @@
+import json
+from backend.api import db
+from backend.api.catalog.models.return_model import Return
+
+
+def insert_return(book_ISBN, book_name, book_author, book_public_company, book_num, book_return_reason, book_seller):
+    # 创建对象
+    return_book = Return(
+        book_ISBN=book_ISBN,
+        book_name=book_name,
+        book_author=book_author,
+        book_public_company=book_public_company,
+        book_num=book_num,
+        book_return_reason=book_return_reason,
+        book_seller=book_seller)
+    # 插入新数据
+    db.session.add(return_book)
+    # 提交数据到数据库
+    db.session.commit()
+
+
+def search_return_book_ISBN(book_ISBN):
+    return_book_list = Return.query.filter(Return.book_ISBN == book_ISBN).all()
+    return return_book_list
+
+
+def search_return_book():
+    # 获得退货清单内所有的内容
+    return_book_list = Return.query.filter().all()
+    # 用来存储转换成json格式的列表
+    return_book_jsonlist = []
+    # 转换格式并存储
+    for each in return_book_list:
+        return_book_jsonlist.append(dict(each))
+    # 转换成json数组
+    return_book_json = json.dumps(return_book_jsonlist, default=str, ensure_ascii=False)
+    return_book_json = json.loads(return_book_json)
+    # 返回数组
+    return return_book_json
+
+
+def update_return_book_book_num(book_ISBN, book_num):
+    # 更新书的数量，通过ISBN以及书的状态查询书的实体
+    return_book = Return.query.filter(Return.book_ISBN == book_ISBN).first()
+    # 更新数量
+    return_book.book_num = return_book.book_num + book_num
+    return_book.book_remainder_num = return_book.book_remainder_num + book_num
+    # 提交数据
+    db.session.commit()
--- backend/api/catalog/models/catalog_model.py
+++ backend/api/catalog/models/catalog_model.py
@@ -3,18 +3,33 @@ from backend.api import db
 
 
 class Catalog(db.Model):
+    # 建立数据表catalog
+    # 包括属性编目号、书的ISBN、书名、书的作者、书的出版社、书的数量、书的剩余数量、书的状态、书的最后修改时间、退货原因
+    # 表名为“ catalog ”
     __tablename__ = 'catalog'
+    # 编目号，通过 create_catalog_id() 来创建
     catalog_id = db.Column(db.String(10), primary_key=True)
+    # 书的ISBN号，每本书一个
     book_ISBN = db.Column(db.String(17), nullable=True)
-    book_name = db.Column(db.String(50), nullable = True)
-    book_author = db.Column(db.String(50),nullable = True)
-    book_public_company = db.Column(db.String(50),nullable = True)
-    book_num = db.Column(db.Integer,nullable = True, default = 1)
-    book_remainder_num = db.Column(db.Integer,nullable = True)
-    book_state = db.Column(db.Integer,nullable = True)
+    # 书的名字
+    book_name = db.Column(db.String(50), nullable=True)
+    # 书的作者
+    book_author = db.Column(db.String(50), nullable=True)
+    # 书的出版社
+    book_public_company = db.Column(db.String(50), nullable=True)
+    # 书的数量
+    book_num = db.Column(db.Integer, nullable=True, default=1)
+    # 书的书库剩余量
+    book_remainder_num = db.Column(db.Integer, nullable=True)
+    # #书的状态，有0/1两种，0表示需要退货，1表示正常可以入库。
+    # book_state = db.Column(db.Integer,nullable = True)
+    # 书的最后修改时间，根据系统时间自动更新
     book_catalog_time = db.Column(db.DateTime, default=datetime.now)
-    book_return_reason = db.Column(db.String(200))
 
+    # #书的退货原因，当book_state = 1 时可以为空，当book_state = 0 时不可为空
+    # book_return_reason = db.Column(db.String(200))
+
+    # 为了方便的转化成字典
     def keys(self):
         return ['catalog_id',
                 'book_ISBN',
@@ -23,9 +38,7 @@ class Catalog(db.Model):
                 'book_public_company',
                 'book_num',
                 'book_remainder_num',
-                'book_state',
-                'book_catalog_time',
-                'book_return_reason']
+                'book_catalog_time']
 
     def __getitem__(self, item):
         return getattr(self, item)
